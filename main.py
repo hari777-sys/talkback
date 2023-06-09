162fa1a4a9f524426370a9ca47b230661be09056
hari <harivicky7777@gmail.com>
2023-06-09 13:16:51 +0530

    hh

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-a=10
-b=0
-if a>b:
-   print("hello")
-else:
-   print("bye")
+import speech_recognition as sr
+listener = sr.Recognizer()
+try:
+    with sr.Microphone() as source:
+        print('listening')
+        voice = listener.listen(source)
+        command = listener.recognize_google(voice)
+        print(command)
+except Exception:
+    print('not working')
