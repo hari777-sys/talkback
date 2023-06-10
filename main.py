b2070bf2d60825d6bdf58f5b9f939c44c141ba2f
hari <harivicky7777@gmail.com>
2023-06-10 12:32:01 +0530

    microphone connection

--- main.py
+++ main.py
@@ -1,10 +1,25 @@
 import pyttsx3
+import speech_recognition as sr
 engine = pyttsx3.init('espeak')
 voices = engine.getProperty('voices')
-engine.setProperty('voice' ,voices[12].id)
+engine.setProperty('voice' ,voices[15].id)
 def speak(audio):
     engine.say(audio)
     engine.runAndWait()
 
+def takecommand():
+    r = sr.Recognizer()
+    with sr.Microphone() as source:
+        print('listening')
+        r.pause_threshold = 1
+        audio = r.listen(source)
+    try:
+        print('wait')
+        query = r.recognize_google(audio,language='en')
+        print('user said', query)
+    except Exception as e:
+        print(e)
+        speak('say  that   again')
 if __name__ == '__main__':
     speak('vanakam    daa    maapla')
+    takecommand()
